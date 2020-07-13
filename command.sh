998a884db0d158e490c30865bef7e9b780decabd
Tianyu Ding <tding1@jhu.edu>
2020-07-13 16:27:25 -0400

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,9 +1,19 @@
-python run.py --backend mobilenetv1 --dataset_name cifar10 --optimizer obproxsg_plus --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
-python run.py --backend resnet18 --dataset_name cifar10 --optimizer obproxsg_plus --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
-python run.py --backend mobilenetv1 --dataset_name fashion_mnist --optimizer obproxsg_plus --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
-python run.py --backend resnet18 --dataset_name fashion_mnist --optimizer obproxsg_plus --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model mobilenetv1 --dataset_name cifar10 --optimizer obproxsg_plus --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model resnet18 --dataset_name cifar10 --optimizer obproxsg_plus --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model mobilenetv1 --dataset_name fashion_mnist --optimizer obproxsg_plus --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model resnet18 --dataset_name fashion_mnist --optimizer obproxsg_plus --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
 
-python run.py --backend mobilenetv1 --dataset_name cifar10 --optimizer obproxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
-python run.py --backend resnet18 --dataset_name cifar10 --optimizer obproxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
-python run.py --backend mobilenetv1 --dataset_name fashion_mnist --optimizer obproxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
-python run.py --backend resnet18 --dataset_name fashion_mnist --optimizer obproxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model mobilenetv1 --dataset_name cifar10 --optimizer obproxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model resnet18 --dataset_name cifar10 --optimizer obproxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model mobilenetv1 --dataset_name fashion_mnist --optimizer obproxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model resnet18 --dataset_name fashion_mnist --optimizer obproxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+
+python run.py --model mobilenetv1 --dataset_name cifar10 --optimizer proxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model resnet18 --dataset_name cifar10 --optimizer proxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model mobilenetv1 --dataset_name fashion_mnist --optimizer proxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model resnet18 --dataset_name fashion_mnist --optimizer proxsg --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+
+python run.py --model mobilenetv1 --dataset_name cifar10 --optimizer rda --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model resnet18 --dataset_name cifar10 --optimizer rda --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model mobilenetv1 --dataset_name fashion_mnist --optimizer rda --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
+python run.py --model resnet18 --dataset_name fashion_mnist --optimizer rda --lambda_ 0.0001 --max_epoch 200 --batch_size 128 -lr 0.1
